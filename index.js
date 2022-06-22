36aa84f47f7cd0d1be57a8290af4aadf21dbdc68
Sam Perozek <samperozek@gmail.com>
2022-06-22 10:33:59 -0500

    Updated Array Iteration using forEach

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const margaritaList = document.querySelector('#margaritaList')
         const drinkList = jsonObject.drinks
+        const commentList = document.querySelector('#comment-list')
 
         let ingredient1 = document.createElement('p')
         let ingredient2 = document.createElement('p')
@@ -25,10 +26,13 @@ document.addEventListener('DOMContentLoaded', () => {
         let ingredient14 = document.createElement('p')
         let ingredient15 = document.createElement('p')
 
-        for (let i=0; i < jsonObject.drinks.length; i++){
+        drinkList.forEach(iterateArray)
+        
+        function iterateArray(jsonObject){
             const margaritaTitle = document.createElement('p')
-            margaritaTitle.textContent = drinkList[i].strDrink
+            margaritaTitle.textContent = jsonObject.strDrink
             margaritaList.append(margaritaTitle)
+            console.log(margaritaTitle)
 
             margaritaTitle.addEventListener('click', () => {
                 
@@ -36,34 +40,34 @@ document.addEventListener('DOMContentLoaded', () => {
                 const margaritaName = document.querySelector('#margarita-name')
                 const ingredientList = document.querySelector('#margarita-ingredients')
                 
-                margaritaImage.src = drinkList[i].strDrinkThumb + "/preview"
-                margaritaName.textContent = drinkList[i].strDrink
+                margaritaImage.src = jsonObject.strDrinkThumb + "/preview"
+                margaritaName.textContent = jsonObject.strDrink
                 
-                drinkList[i].strIngredient1 === null ? ingredient1.remove():ingredient1.textContent = drinkList[i].strMeasure1 + " " + drinkList[i].strIngredient1
-                drinkList[i].strIngredient2 === null ? ingredient2.remove():ingredient2.textContent = drinkList[i].strMeasure2 + " " + drinkList[i].strIngredient2
-                drinkList[i].strIngredient3 === null ? ingredient3.remove():ingredient3.textContent = drinkList[i].strMeasure3 + " " + drinkList[i].strIngredient3
-                drinkList[i].strIngredient4 === null ? ingredient4.remove():ingredient4.textContent = drinkList[i].strMeasure4 + " " + drinkList[i].strIngredient4
-                drinkList[i].strIngredient5 === null ? ingredient5.remove():ingredient5.textContent = drinkList[i].strMeasure5 + " " + drinkList[i].strIngredient5
-                drinkList[i].strIngredient6 === null ? ingredient6.remove():ingredient6.textContent = drinkList[i].strMeasure6 + " " + drinkList[i].strIngredient6
-                drinkList[i].strIngredient7 === null ? ingredient7.remove():ingredient7.textContent = drinkList[i].strMeasure7 + " " + drinkList[i].strIngredient7
-                drinkList[i].strIngredient8 === null ? ingredient8.remove():ingredient8.textContent = drinkList[i].strMeasure8 + " " + drinkList[i].strIngredient8
-                drinkList[i].strIngredient9 === null ? ingredient9.remove():ingredient9.textContent = drinkList[i].strMeasure9 + " " + drinkList[i].strIngredient9
-                drinkList[i].strIngredient10 === null ? ingredient10.remove():ingredient10.textContent = drinkList[i].strMeasure10 + " " + drinkList[i].strIngredient10
-                drinkList[i].strIngredient11 === null ? ingredient11.remove():ingredient11.textContent = drinkList[i].strMeasure11 + " " + drinkList[i].strIngredient11
-                drinkList[i].strIngredient12 === null ? ingredient12.remove():ingredient12.textContent = drinkList[i].strMeasure12 + " " + drinkList[i].strIngredient12
-                drinkList[i].strIngredient13 === null ? ingredient13.remove():ingredient13.textContent = drinkList[i].strMeasure13 + " " + drinkList[i].strIngredient13
-                drinkList[i].strIngredient14 === null ? ingredient14.remove():ingredient14.textContent = drinkList[i].strMeasure14 + " " + drinkList[i].strIngredient14
-                drinkList[i].strIngredient15 === null ? ingredient15.remove():ingredient15.textContent = drinkList[i].strMeasure15 + " " + drinkList[i].strIngredient15
+                jsonObject.strIngredient1 === null ? ingredient1.remove():ingredient1.textContent = jsonObject.strMeasure1 + " " + jsonObject.strIngredient1
+                jsonObject.strIngredient2 === null ? ingredient2.remove():ingredient2.textContent = jsonObject.strMeasure2 + " " + jsonObject.strIngredient2
+                jsonObject.strIngredient3 === null ? ingredient3.remove():ingredient3.textContent = jsonObject.strMeasure3 + " " + jsonObject.strIngredient3
+                jsonObject.strIngredient4 === null ? ingredient4.remove():ingredient4.textContent = jsonObject.strMeasure4 + " " + jsonObject.strIngredient4
+                jsonObject.strIngredient5 === null ? ingredient5.remove():ingredient5.textContent = jsonObject.strMeasure5 + " " + jsonObject.strIngredient5
+                jsonObject.strIngredient6 === null ? ingredient6.remove():ingredient6.textContent = jsonObject.strMeasure6 + " " + jsonObject.strIngredient6
+                jsonObject.strIngredient7 === null ? ingredient7.remove():ingredient7.textContent = jsonObject.strMeasure7 + " " + jsonObject.strIngredient7
+                jsonObject.strIngredient8 === null ? ingredient8.remove():ingredient8.textContent = jsonObject.strMeasure8 + " " + jsonObject.strIngredient8
+                jsonObject.strIngredient9 === null ? ingredient9.remove():ingredient9.textContent = jsonObject.strMeasure9 + " " + jsonObject.strIngredient9
+                jsonObject.strIngredient10 === null ? ingredient10.remove():ingredient10.textContent = jsonObject.strMeasure10 + " " + jsonObject.strIngredient10
+                jsonObject.strIngredient11 === null ? ingredient11.remove():ingredient11.textContent = jsonObject.strMeasure11 + " " + jsonObject.strIngredient11
+                jsonObject.strIngredient12 === null ? ingredient12.remove():ingredient12.textContent = jsonObject.strMeasure12 + " " + jsonObject.strIngredient12
+                jsonObject.strIngredient13 === null ? ingredient13.remove():ingredient13.textContent = jsonObject.strMeasure13 + " " + jsonObject.strIngredient13
+                jsonObject.strIngredient14 === null ? ingredient14.remove():ingredient14.textContent = jsonObject.strMeasure14 + " " + jsonObject.strIngredient14
+                jsonObject.strIngredient15 === null ? ingredient15.remove():ingredient15.textContent = jsonObject.strMeasure15 + " " + jsonObject.strIngredient15
 
                 ingredientList.append(ingredient1,ingredient2,ingredient3,ingredient4,ingredient5,ingredient6,ingredient7,ingredient8,ingredient9,ingredient10,ingredient11,ingredient12,ingredient13,ingredient14,ingredient15)
             })
         }
+
         document.querySelector('#comment-form').addEventListener('submit', (event) => {
             event.preventDefault()
-            let newComment = document.createElement('p') 
-            newComment = event.target.review.value
-
-            console.log(newComment)
+            let newComment = document.createElement('li') 
+            newComment = `${event.target.review.value}\n`
+            commentList.append(newComment)            
         })
               
     }
