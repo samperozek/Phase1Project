2e291dc6f78fe80dede782562e6bd1e67241c749
Sam Perozek <samperozek@gmail.com>
2022-06-23 15:09:03 -0500

    Added random cocktail generator and cleaned up comments to disappear

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 margaritaImage.src = jsonObject.strDrinkThumb + "/preview"
                 margaritaName.textContent = jsonObject.strDrink
                 
-                jsonObject.strIngredient1 === null ? ingredient1.remove():ingredient1.textContent = jsonObject.strMeasure1 + " " + jsonObject.strIngredient1
-                jsonObject.strIngredient2 === null ? ingredient2.remove():ingredient2.textContent = jsonObject.strMeasure2 + " " + jsonObject.strIngredient2
-                jsonObject.strIngredient3 === null ? ingredient3.remove():ingredient3.textContent = jsonObject.strMeasure3 + " " + jsonObject.strIngredient3
-                jsonObject.strIngredient4 === null ? ingredient4.remove():ingredient4.textContent = jsonObject.strMeasure4 + " " + jsonObject.strIngredient4
+                jsonObject.strIngredient1 === null ? ingredient1.innerHTML = ``:ingredient1.textContent = jsonObject.strMeasure1 + " " + jsonObject.strIngredient1
+                jsonObject.strIngredient2 === null ? ingredient2.innerHTML = ``:ingredient2.textContent = jsonObject.strMeasure2 + " " + jsonObject.strIngredient2
+                jsonObject.strIngredient3 === null ? ingredient3.innerHTML = ``:ingredient3.textContent = jsonObject.strMeasure3 + " " + jsonObject.strIngredient3
+                jsonObject.strIngredient4 === null ? ingredient4.innerHTML = ``:ingredient4.textContent = jsonObject.strMeasure4 + " " + jsonObject.strIngredient4
                 jsonObject.strIngredient5 === null ? ingredient5.innerHTML = ``:ingredient5.textContent = jsonObject.strMeasure5 + " " + jsonObject.strIngredient5
                 jsonObject.strIngredient6 === null ? ingredient6.innerHTML = ``:ingredient6.textContent = jsonObject.strMeasure6 + " " + jsonObject.strIngredient6
                 jsonObject.strIngredient7 === null ? ingredient7.innerHTML = ``:ingredient7.textContent = jsonObject.strMeasure7 + " " + jsonObject.strIngredient7
@@ -80,10 +80,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 margaritaImageRandom.src = drinkListRandom.strDrinkThumb + "/preview"
                 margaritaNameRandom.textContent = drinkListRandom.strDrink
                 
-                drinkListRandom.strIngredient1 === null ? ingredient1.remove():ingredient1.textContent = drinkListRandom.strMeasure1 + " " + drinkListRandom.strIngredient1
-                drinkListRandom.strIngredient2 === null ? ingredient2.remove():ingredient2.textContent = drinkListRandom.strMeasure2 + " " + drinkListRandom.strIngredient2
-                drinkListRandom.strIngredient3 === null ? ingredient3.remove():ingredient3.textContent = drinkListRandom.strMeasure3 + " " + drinkListRandom.strIngredient3
-                drinkListRandom.strIngredient4 === null ? ingredient4.remove():ingredient4.textContent = drinkListRandom.strMeasure4 + " " + drinkListRandom.strIngredient4
+                drinkListRandom.strIngredient1 === null ? ingredient1.innerHTML = ``:ingredient1.textContent = drinkListRandom.strMeasure1 + " " + drinkListRandom.strIngredient1
+                drinkListRandom.strIngredient2 === null ? ingredient2.innerHTML = ``:ingredient2.textContent = drinkListRandom.strMeasure2 + " " + drinkListRandom.strIngredient2
+                drinkListRandom.strIngredient3 === null ? ingredient3.innerHTML = ``:ingredient3.textContent = drinkListRandom.strMeasure3 + " " + drinkListRandom.strIngredient3
+                drinkListRandom.strIngredient4 === null ? ingredient4.innerHTML = ``:ingredient4.textContent = drinkListRandom.strMeasure4 + " " + drinkListRandom.strIngredient4
                 drinkListRandom.strIngredient5 === null ? ingredient5.innerHTML = ``:ingredient5.textContent = drinkListRandom.strMeasure5 + " " + drinkListRandom.strIngredient5
                 drinkListRandom.strIngredient6 === null ? ingredient6.innerHTML = ``:ingredient6.textContent = drinkListRandom.strMeasure6 + " " + drinkListRandom.strIngredient6
                 drinkListRandom.strIngredient7 === null ? ingredient7.innerHTML = ``:ingredient7.textContent = drinkListRandom.strMeasure7 + " " + drinkListRandom.strIngredient7
